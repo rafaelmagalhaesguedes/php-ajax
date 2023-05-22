86e7d98410530fe852acb2b8f638bda3f7c2fdef
Rafael Guedes <rafaelmagalhaes1@live.com>
2023-05-22 09:56:00 -0300

    Adicionado bloco Try Catch e consultas parametrizadas para evitar SQL injection no código em excluir_usuario.php

--- excluir_usuario.php
+++ excluir_usuario.php
@@ -1,25 +1,37 @@
 <?php
 require_once "config.php";
 
+// Conexão com a base de dados
 $bd = new BaseDeDados();
 $bd->connect();
 $conexao = $bd->getConnection();
 
-if (mysqli_connect_errno()) {
-    echo "Falha ao conectar com o MySQL: " . mysqli_connect_error();
-    exit();
-}
+try {
+    if (mysqli_connect_errno()) {
+        throw new Exception("Falha ao conectar com o MySQL: " . mysqli_connect_error());
+    }
 
-$id = $_POST["id"];
+    // Recebe o valor do ID
+    $id = $_POST["id"];
 
-$sql = "DELETE FROM usuarios WHERE id=$id";
+    // Utilize prepared statements para evitar SQL injection
+    $sql = "DELETE FROM usuarios WHERE id = ?";
+    
+    /* Em seguida, usamos o método bind_param para vincular o 
+    valor da variável $id ao espaço reservado na consulta. 
+    Isso garante que o valor seja tratado como um parâmetro 
+    e não como parte do comando SQL. */
+    $stmt = $conexao->prepare($sql);
+    $stmt->bind_param("i", $id);
 
-if (mysqli_query($conexao, $sql)) {
-    echo "Usuário excluído com sucesso.";
-} else {
-    echo "Erro ao excluir usuário: " . mysqli_error($conexao);
+    if ($stmt->execute()) {
+        echo "Usuário excluído com sucesso.";
+    } else {
+        throw new Exception("Erro ao excluir usuário: " . $stmt->error);
+    }
+} catch (Exception $e) {
+    echo "Ocorreu um erro: " . $e->getMessage();
 }
 
 $conexao->close();
-
 ?>
