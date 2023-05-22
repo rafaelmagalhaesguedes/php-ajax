cbf5146bdae496cd712c686db5bfb944462a8c38
Rafael Guedes <rafaelmagalhaes1@live.com>
2023-05-22 10:21:00 -0300

    Comentários inseridos ao código

--- config.php
+++ config.php
@@ -1,7 +1,8 @@
 <?php
-
+// Classe de conexão com o banco de dados
 class BaseDeDados {
 
+    // Dados de conexão com o Banco
     public function __construct() {
         $this->host = 'localhost';
         $this->username = 'root';
@@ -9,6 +10,7 @@ class BaseDeDados {
         $this->database = 'crud';
     }
 
+    // Método conn
     public function connect() {
         $this->conn = mysqli_connect($this->host, $this->username, $this->password, $this->database);
         if (!$this->conn) {
@@ -16,10 +18,12 @@ class BaseDeDados {
         }
     }
 
+    // Método close
     public function close() {
         mysqli_close($this->conn);
     }
 
+    // método GetConnection
     public function getConnection() {
         return $this->conn;
     }
