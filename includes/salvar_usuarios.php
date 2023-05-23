ab3dd1c53b14811b16a451f807d013563ab6f71c
Rafael Guedes <rafaelmagalhaes1@live.com>
2023-05-23 18:19:36 -0300

    Adicionando bloco Try/Catch ao código para tratar possíveis erros

--- includes/salvar_usuarios.php
+++ includes/salvar_usuarios.php
@@ -1,41 +1,46 @@
 <?php
 require_once "config.php";
 
-$bd = new BaseDeDados();
-$bd->connect();
-$conexao = $bd->getConnection();
+try{
+    $bd = new BaseDeDados();
+    $bd->connect();
+    $conexao = $bd->getConnection();
 
-if (mysqli_connect_errno()) {
-    echo "Falha ao conectar com o MySQL: " . mysqli_connect_error();
-    exit();
+    if (mysqli_connect_errno()) {
+        echo "Falha ao conectar com o MySQL: " . mysqli_connect_error();
+        exit();
+    }
+
+    $id = $_POST["id"];
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+
+    /* Nesse código, utilizamos prepared statements com o uso de placeholders (?) 
+    nas consultas SQL. Em seguida, preparamos a consulta usando $conexao->prepare($sql)
+    e usamos $stmt->bind_param() para vincular os valores aos placeholders na ordem correta.
+    Dessa forma, os valores de $nome, $email e $id são tratados de forma segura e não permitem
+    a execução de código malicioso.
+    */
+    if ($id != "") {
+        $sql = "UPDATE usuarios SET nome=?, email=? WHERE id=?";
+        $stmt = $conexao->prepare($sql);
+        $stmt->bind_param("ssi", $nome, $email, $id);
+    } else {
+        $sql = "INSERT INTO usuarios (nome, email) VALUES (?, ?)";
+        $stmt = $conexao->prepare($sql);
+        $stmt->bind_param("ss", $nome, $email);
+    }
+
+    if ($stmt->execute()) {
+        echo "Usuário salvo com sucesso.";
+    } else {
+        throw new Exception("Erro ao salvar usuário: " . $stmt->error);
+    }
+
+    $stmt->close();
+    $conexao->close();
 }
-
-$id = $_POST["id"];
-$nome = $_POST["nome"];
-$email = $_POST["email"];
-
-/* Nesse código, utilizamos prepared statements com o uso de placeholders (?) 
-nas consultas SQL. Em seguida, preparamos a consulta usando $conexao->prepare($sql)
-e usamos $stmt->bind_param() para vincular os valores aos placeholders na ordem correta.
-Dessa forma, os valores de $nome, $email e $id são tratados de forma segura e não permitem
-a execução de código malicioso.
-*/
-if ($id != "") {
-    $sql = "UPDATE usuarios SET nome=?, email=? WHERE id=?";
-    $stmt = $conexao->prepare($sql);
-    $stmt->bind_param("ssi", $nome, $email, $id);
-} else {
-    $sql = "INSERT INTO usuarios (nome, email) VALUES (?, ?)";
-    $stmt = $conexao->prepare($sql);
-    $stmt->bind_param("ss", $nome, $email);
+catch (Exception $e) {
+        echo "Ocorreu um erro: " . $e->getMessage();
 }
-
-if ($stmt->execute()) {
-    echo "Usuário salvo com sucesso.";
-} else {
-    echo "Erro ao salvar usuário: " . $stmt->error;
-}
-
-$stmt->close();
-$conexao->close();
 ?>
